5c70a9421eb70143d75f4603e866c5030c35b251
Kieran Wood <kieran@foxit.co.nz>
2021-10-17 08:30:30 +1300

    Fixed issues with relative import

---
+++ __init__.py
@@ -0,0 +1,19 @@
+bl_info = {
+    "name": "FFXV Utilities",
+    "blender": (2, 79, 0),
+    "category": "Object"
+}
+
+from .cleanup.register import (register_cleanup, unregister_cleanup)
+from .animation.register import (register_animation, unregister_animation)
+from .weight.register import (register_weight, unregister_weight)
+
+def register():
+    register_cleanup()
+    register_animation()
+    register_weight()
+
+def unregister():
+    unregister_weight()
+    unregister_animation()
+    unregister_cleanup()
